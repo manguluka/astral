ade2db7f7246ecaaa66cdce658bc35bd2c811801
manguluka <tali@manguluka.com>
2017-10-28 17:07:12 -0700

    imwe

---
+++ src/ascii_art.rs
@@ -0,0 +1,64 @@
+pub static MOON_IMAGE_NEW: &str = "
+    _..._
+  .:::::::.
+ :::::::::::
+ :::::::::::
+ `:::::::::'
+   `':::'`
+";
+pub static MOON_IMAGE_WAXING_C: &str = "
+    _..._
+  .::::. `.
+ :::::::.  :
+ ::::::::  :
+ `::::::' .'
+   `'::'-'
+";
+pub static MOON_IMAGE_FIRST_Q: &str = "
+    _..._
+  .::::  `.
+ ::::::    :
+ ::::::    :
+ `:::::   .'
+   `'::.-'
+";
+pub static MOON_IMAGE_WAXING_G: &str = "
+    _..._
+  .::'   `.
+ :::       :
+ :::       :
+ `::.     .'
+   `':..-'
+";
+pub static MOON_IMAGE_FULL: &str = "
+    _..._
+  .'     `.
+ :         :
+ :         :
+ `.       .'
+   `-...-'
+";
+pub static MOON_IMAGE_WANING_G: &str = "
+    _..._
+  .'   `::.
+ :       :::
+ :       :::
+ `.     .::'
+   `-..:''
+";
+pub static MOON_IMAGE_LAST_Q: &str = "
+    _..._
+  .'  ::::.
+ :    ::::::
+ :    ::::::
+ `.   :::::'
+   `-.::''
+";
+pub static MOON_IMAGE_WANING_C: &str = "
+    _..._
+  .' .::::.
+ :  ::::::::
+ :  ::::::::
+ `. '::::::'
+   `-.::''
+";
